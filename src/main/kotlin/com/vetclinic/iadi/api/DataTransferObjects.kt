1fadfdac42ed808b6cfa26c8df310923759a2b22
João Pacheco <dblamk@hotmail.com>
2019-10-03 15:29:50 +0100

    forgot parenthesis

--- src/main/kotlin/com/vetclinic/iadi/api/DataTransferObjects.kt
+++ src/main/kotlin/com/vetclinic/iadi/api/DataTransferObjects.kt
@@ -1,5 +1,6 @@
 package com.vetclinic.iadi.api
 
-data class PetDTO(val id:Number, val name: String, val species: String, val age:Number, val owner:String
+data class PetDTO(val id:Number, val name: String, val species: String, val age:Number, val owner:String)
+
 data class ClientDTO(val username:String, val password:String)
 
