c13da70e69486217d3a2b7e1ba50c44ca346f2c0
Pedro Pais <pg.pais@campus.fct.unl.pt>
2019-10-11 13:15:46 +0100

    sketch of client DB

--- src/main/kotlin/com/vetclinic/iadi/model/DataAccessObjects.kt
+++ src/main/kotlin/com/vetclinic/iadi/model/DataAccessObjects.kt
@@ -1,10 +1,12 @@
 package com.vetclinic.iadi.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
+import javax.persistence.*
 
 //daos are for databases
 
 @Entity
 data class PetDAO(@Id @GeneratedValue val id:Long, val name:String, val species:String)
+
+@Entity
+data class ClientDAO(@Id val username:String, val name:String, val password:String,
+                     @OneToMany(mappedBy = "username") val pet:List<PetDAO>)
