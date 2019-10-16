22e782c02ef4ee2af4a97f5710fc753b91e98d2b
Pedro Pais <pg.pais@campus.fct.unl.pt>
2019-10-16 16:55:20 +0100

    Mixed provided code with ours

---
+++ src/main/kotlin/com/vetclinic/iadi/api/Exceptions.kt
@@ -0,0 +1,7 @@
+package pt.unl.fct.di.iadi.vetclinic.api
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class HTTPNotFoundException(s:String) : Exception(s)
