935b931872c745c5a41f6f0102347de8376f1f50
Pedro Pais <pg.pais@campus.fct.unl.pt>
2019-10-20 14:29:41 +0100

    UnRegisteredUserController skeleton - needs methods

--- src/main/kotlin/com/vetclinic/iadi/api/UnRegUserController.kt
+++ src/main/kotlin/com/vetclinic/iadi/api/UnRegUserController.kt
@@ -1,4 +1,39 @@
 package com.vetclinic.iadi.api
 
-class UnRegUserController {
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
+import org.springframework.web.bind.annotation.*
+
+
+@Api(value = "VetClinic Management System - Access API",
+        description = "Management access operations in the IADI 2019 Pet Clinic")
+
+@RestController
+@RequestMapping("/session")
+class UnRegUserController { //TODO: add service (call it something related with accounts?)
+
+    @ApiOperation(value = "Login with given user") // TODO: should probably return a token
+    @ApiResponses(value = [
+        ApiResponse(code = 200, message = "Successfully logged in"), //201 for successful token creation?
+        ApiResponse(code = 404, message = "Could not find provided user"),
+        ApiResponse(code = 403, message = "Could not login with provided information")
+
+    ])
+    @PostMapping("/login")
+    fun login(@RequestBody username:String, @RequestBody password:String) =
+            {
+                // TODO: execute login, maybe encrypt password here
+            }
+
+    @ApiOperation(value = "Register a new user")
+    @ApiResponses(value = [
+        ApiResponse(code = 201, message = "Successfully created a new user"),
+        ApiResponse(code = 403, message = "You cannot access this resource (maybe already logged in?)")
+    ])
+    @PostMapping("/login")
+    fun register(@RequestBody user:ClientDTO) = {
+            // TODO: create new ClientDAO and save it in database?
+    }
 }
