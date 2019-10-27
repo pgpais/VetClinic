fd9221cafdbceace8eaa4782dff5948a84653449
João Pacheco <dblamk@hotmail.com>
2019-10-27 19:34:56 +0000

    ok

--- src/main/kotlin/com/vetclinic/iadi/api/VetController.kt
+++ src/main/kotlin/com/vetclinic/iadi/api/VetController.kt
@@ -5,12 +5,14 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
 @Api(value = "VetClinic Management System - Veterinarian API",
         description = "Management operations of Veterinarians in the IADI 2019 Pet Clinic")
 
 @RestController
+@PreAuthorize("hasRole('ROLE_VETS')")
 @RequestMapping("/vets")
 class VetController (val vets:VetService) {
     //TODO: "Veterinarians can also check the information of their clients, appointments, and of all pets"
