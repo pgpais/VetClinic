d9c0815e142a291603372c037e2f31f4d646ee30
João Pacheco <dblamk@hotmail.com>
2019-10-27 23:28:27 +0000

    New bois in

---
+++ src/main/kotlin/com/vetclinic/iadi/config/ClientCustomInfo.kt
@@ -0,0 +1,66 @@
+package com.vetclinic.iadi.config
+
+import com.vetclinic.iadi.services.AdminService
+import com.vetclinic.iadi.services.ClientService
+import com.vetclinic.iadi.services.VetService
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+class ClientCustomInfo(
+        private val username:String,
+        private val password:String,
+        private val someAuthorities:MutableCollection<out GrantedAuthority>) : UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = someAuthorities
+
+    override fun isEnabled(): Boolean = true
+
+    override fun getUsername(): String = username
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun getPassword(): String = password
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+}
+
+@Transactional
+@Service
+class CustomClientInfoService(
+        val clientService: ClientService,
+        val adminService:AdminService,
+        val vetService: VetService
+) : UserDetailsService {
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+
+        username?.let {
+            val clientDAO = clientService.getClientByUsername(it)
+            val adminDAO = adminService.getAdminByUsername(it)
+            val veterinarianDAO = vetService.getVetByUsername(it)
+
+
+            if( clientDAO.isPresent) {
+                return ClientCustomInfo(clientDAO.get().username, clientDAO.get().pass, mutableListOf(SimpleGrantedAuthority("ROLE_CLIENT")))
+
+            } else if (adminDAO.isPresent) {
+               return  ClientCustomInfo(adminDAO.get().username, adminDAO.get().pass,
+                        mutableListOf(SimpleGrantedAuthority("ROLE_ADMIN")))
+
+            } else if (veterinarianDAO.isPresent){
+                return ClientCustomInfo(veterinarianDAO.get().username, veterinarianDAO.get().pass,
+                        mutableListOf(SimpleGrantedAuthority("ROLE_VETERINARIAN")))
+            } else
+                throw UsernameNotFoundException(username)
+            }
+
+        throw UsernameNotFoundException(username)
+    }
+}
---
+++ src/main/kotlin/com/vetclinic/iadi/config/Security.kt
@@ -0,0 +1,50 @@
+package com.vetclinic.iadi.config
+
+import com.vetclinic.iadi.services.ClientService
+import com.vetclinic.iadi.services.RegisteredUserService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
+class Security(val customClientinfo:CustomClientInfoService,
+                       val clients: ClientService) : WebSecurityConfigurerAdapter()
+{
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/v2/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.POST,"/register").permitAll()
+                .antMatchers("/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(UserPasswordAuthenticationFilterToJWT ("/login", super.authenticationManagerBean()),
+                        BasicAuthenticationFilter::class.java)
+                .addFilterBefore(ClientPasswordSignUpFilterToJWT ("/register", clients),
+                        BasicAuthenticationFilter::class.java)
+                .addFilterBefore(JWTAuthenticationFilter(),
+                        BasicAuthenticationFilter::class.java)
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(customClientinfo).passwordEncoder(BCryptPasswordEncoder())
+    }
+
+
+
+}
