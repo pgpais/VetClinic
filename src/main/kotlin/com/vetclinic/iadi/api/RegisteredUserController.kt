8aaeecce9e2506a660a356dcdae1b36470b64a9e
Pedro Pais <pg.pais@campus.fct.unl.pt>
2019-10-28 02:01:39 +0000

    UserDAO update is ready

--- src/main/kotlin/com/vetclinic/iadi/api/RegisteredUserController.kt
+++ src/main/kotlin/com/vetclinic/iadi/api/RegisteredUserController.kt
@@ -14,34 +14,9 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/home/{username}")
 class RegisteredUserController (val regUserService: RegisteredUserService){
 
-
-    /*@ApiOperation(value = "Get the details of your pet by Id", response = PetDTO::class)
-    @ApiResponses(value = [
-        ApiResponse(code = 200, message = "Successfully retrieved pet details"),
-        ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-        ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-        ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-    ])
-    @GetMapping("/pets/{id}")
-    fun getPetById(@PathVariable id:Long, @PathVariable username:String){}
-
-
-    @ApiOperation(value = "Get the details of your pet by Id", response = List::class)
-    @ApiResponses(value = [
-        ApiResponse(code = 200, message = "Successfully retrieved pet details"),
-        ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-        ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-        ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-    ])
-    @GetMapping("/pets")
-    fun getAllPets(@PathVariable username:String){}*/// This should not be here
-
     @ApiOperation(value = "Update user info")
     @PutMapping("")
     fun updateInfo(@PathVariable username: String, @RequestBody newUser:UserDTO){
         regUserService.updateInfo(username, newUser)
-
     }
-
-    //TODO: log out?
 }
