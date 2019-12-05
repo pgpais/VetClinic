d7069552aaf465d32acbecc6672573bf189ab821
Pedro Pais <pg.pais@campus.fct.unl.pt>
2019-12-05 15:51:45 +0000

    some skeleton work layed out

--- src/main/frontend/src/Home/ClientHome/index.tsx
+++ src/main/frontend/src/Home/ClientHome/index.tsx
@@ -2,15 +2,22 @@ import React, {ChangeEvent, useEffect, useState} from "react";
 import {FilteredPetList, PetRegistration} from "../../Pets";
 import {Appointment, AppointmentList, ProtoAppointmentList} from "../../Appointment";
 
+const AppointmentForm = () => {
 
+    return <>
+        </>
+}
 
 const ShowAppointments = () => {
 
     let [isShowing, setIsShowing] = useState(true);
+    let [isCreatingAppointment, setCreatingAppointment] = useState(false);
 
     return <>
             <button onClick={() => setIsShowing(!isShowing)}>{isShowing? "Hide Appointments" : "Show Appointments"}</button>
             {isShowing && <AppointmentList/>}
+            <button onClick={() => setCreatingAppointment(!isCreatingAppointment)}>{isCreatingAppointment? "Cancel":"Create Appointment"} </button>
+            {isCreatingAppointment && <AppointmentForm/>}
         </>
 };
 
@@ -18,9 +25,9 @@ const ProtoClientHome = () => {
 
 
     return <>
-        <ShowAppointments/>
         <FilteredPetList/>
         <PetRegistration/>
+        <ShowAppointments/>
     </>;
 };
 
